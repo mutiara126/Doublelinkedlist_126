0e90cbabf2f518a5123999fa58fb39dfd9360339
mutiara126 <salsamutiara1812@gmail.com>
2023-05-25 20:55:50 +0800

    untuk menghapus variabel double LL

--- Doublelinkedlist_126/Doublelinkedlist_126.cpp
+++ Doublelinkedlist_126/Doublelinkedlist_126.cpp
@@ -136,6 +136,20 @@ void DoubleLinkedlist::revtraverse() {
 	}
 }
 
+void DoubleLinkedlist::hapus() {
+	if (listEmpty()) {
+		cout << "\nList is empty" << endl;
+
+	}
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedlist::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number " << rollNo << "deleted" << endl;
+}
 
 
 
